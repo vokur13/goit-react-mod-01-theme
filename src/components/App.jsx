19439cb419bdbd1796bf06830de927a5cebbf941
vokur13 <vokur13@gmail.com>
2022-09-21 19:33:38 +0300

    example added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
 
 const Text = styled.p`
-  color: red;
+  color: ${props => props.theme.colors.accent};
+  font-famile: ${props => props.theme.fonts.body};
 `;
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <Text>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores aperiam
+      omnis eveniet numquam tenetur laudantium rem eum, laboriosam dolores
+      voluptatum voluptate obcaecati dolore pariatur ipsum? Saepe ex quis at
+      adipisci?
+    </Text>
+  );
 };
--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: { text: 'black', accent: 'orangered' },
+  fonts: {
+    body: 'Helvetica, sans-serif',
+    title: ' Tacoma,sans-serif',
+    monospace: 'monospace',
+  },
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
