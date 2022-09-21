beca5c67a1b1daab33a08955bd80b72197e3e345
vokur13 <vokur13@gmail.com>
2022-09-21 19:17:12 +0300

    project init

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import styled from 'styled-components';
+
+const Text = styled.p`
+  color: red;
+`;
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <div></div>;
 };
